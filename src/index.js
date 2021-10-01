6e2ce3f8430f1c7cace9e7342b2044edf406ec28
Kate Beard <asouthernbellein@gmail.com>
2021-10-01 15:13:30 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
-import freesewing from '@freesewing/core'
-import plugins from '@freesewing/plugin-bundle'
-import config from '../config'
-import draftNeckBase from './neckBase'
-import draftBack from './back'
-import draftBase from './base'
-import draftFront from './front'
-import draftSleeve from './sleeve'
+import freesewing from "@freesewing/core";
+import plugins from "@freesewing/plugin-bundle";
+import config from "../config";
+import draftNeckBase from "./neckBase";
+import draftBack from "./back";
+import draftBase from "./base";
+import draftFront from "./front";
+import draftSleeve from "./sleeve";
+
+console.log("testing modified file");
 
 // Create new design
-const Pattern = new freesewing.Design(config, plugins)
+const Pattern = new freesewing.Design(config, plugins);
 
 // Attach the draft methods to the prototype
-Pattern.prototype.draftNeckBase = draftNeckBase
-Pattern.prototype.draftBase = draftBase
-Pattern.prototype.draftBack = draftBack
-Pattern.prototype.draftFront = draftFront
-Pattern.prototype.draftSleeve = draftSleeve
+Pattern.prototype.draftNeckBase = draftNeckBase;
+Pattern.prototype.draftBase = draftBase;
+Pattern.prototype.draftBack = draftBack;
+Pattern.prototype.draftFront = draftFront;
+Pattern.prototype.draftSleeve = draftSleeve;
 
-export default Pattern
+export default Pattern;
